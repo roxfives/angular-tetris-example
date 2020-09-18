0e4896b763588b471596161d755322405e381a6c
Ricardo Chagas <ricardo.chagas2@outlook.com>
2020-09-18 18:33:12 -0300

    Loads the board asynchronously

--- projects/tetris-app/src/app/features/playground/board/board.component.ts
+++ projects/tetris-app/src/app/features/playground/board/board.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { combineLatest } from 'rxjs';
 
-import { Observable } from 'rxjs';
+import { first, tap, finalize } from 'rxjs/operators';
 
 import { BoardService } from '../../../core/service/board.service';
 
@@ -12,15 +13,55 @@ import { BoardService } from '../../../core/service/board.service';
 })
 export class BoardComponent implements OnInit {
 
-  columns$!: Observable<number>;
-  rows$!: Observable<number>;
-  block$!: Observable<number>;
+  columns!: number;
+  rows!: number;
+  blockSize!: number;
+  
+  @ViewChild('board')
+  board!: ElementRef<HTMLCanvasElement>;
+
+  ctx!: CanvasRenderingContext2D;
+  points!: number;
+  lines!: number;
+  level!: number;
   
   constructor(private boardService: BoardService) { }
 
   ngOnInit(): void {
-    this.columns$ = this.boardService.columns$;
-    this.rows$ = this.boardService.rows$;
-    this.block$ = this.boardService.blockSize$;
+    this.initBoardParams().pipe(
+        finalize(() => 
+          this.initBoard())
+      )
+      .subscribe();
+  }
+
+  private initBoard() {
+    // Get the 2D context that we draw on.
+    const aux = this.board.nativeElement.getContext('2d');
+    if (aux) {
+      this.ctx = aux;
+    } else {
+      console.log('Canvas context is null');
+      return;
+    }
+
+    // Calculate size of canvas from constants.
+    this.ctx.canvas.width = this.columns * this.blockSize;
+    this.ctx.canvas.height = this.rows * this.blockSize;
+  }
+
+  private initBoardParams() {
+    const column$ = this.boardService.columns$.pipe(first());
+    const row$ = this.boardService.rows$.pipe(first());
+    const block$ = this.boardService.blockSize$.pipe(first());
+
+    return combineLatest([column$, row$, block$])
+      .pipe(
+        tap(([cols, rows, blocks]) => {
+          this.columns = cols;
+          this.rows = rows;
+          this.blockSize = blocks;
+        })
+      );
   }
 }
