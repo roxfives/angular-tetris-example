aed9f027572376d18d5cefaff9e70e1d5a8ea788
Ricardo Chagas <ricardo.chagas2@outlook.com>
2020-09-18 18:32:50 -0300

    Removes useless import

--- projects/tetris-app/src/app/core/state/board-settings.state.ts
+++ projects/tetris-app/src/app/core/state/board-settings.state.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { State, Selector } from '@ngxs/store';
 
-import { of } from 'rxjs';
-
 
 export class BoardSettingsStateModel {
   public board: {
